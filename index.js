629d8d1b6fac011c3cdb247fed2f02fb0c17ab25
virakun <93888991+virakun@users.noreply.github.com>
2022-02-21 06:19:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('BOT BY\n ELYAS', {
+CFonts.say('BOT VVIP BY\n DOI', {
   colors: ['blueBright','yellowBright'],                                        font: 'block',
   align: 'center',
 })
-CFonts.say(`BOT WHATSAPP BY EYAS`, {                                            
+CFonts.say(`BOT WHATSAPP BY DOU`, {                                            
 colors: ['yellow'],                                                           
 font: 'console',                                                              align: 'center',
 })
