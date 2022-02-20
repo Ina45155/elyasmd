78df593f7587f520a1fc7dae294567da9cd4d2fd
virakun <93888991+virakun@users.noreply.github.com>
2022-02-21 06:35:40 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -8,13 +8,13 @@ const q = {
 	},
 	"message": {
 		"contactMessage": {
-			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285643354073:+62 856-4335-4073\nEND:VCARD"
+			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;BOT Gantwng;;;FN:BOT Ganteng\nORG:BOT Ganteng;\nTEL;type=CELL;type=VOICE;waid=6281283408836:+62 812-8340-8836\nEND:VCARD"
 		}
 	},
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6285643354073', 'elyas', m)
+conn.sendContact(m.chat, '6281283408836', 'DOI', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
